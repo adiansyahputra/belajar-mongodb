f73de0ae05e6110f6b729cb886aee848b582c6a2
Adiansyah Putra <adiansyahhputra@gmail.com>
2022-10-07 17:46:45 +0700

    Belajar MongoDB

--- scripts/update.js
+++ scripts/update.js
@@ -1,81 +1,92 @@
 // update products set category = "food" where _id = 1
-db.products.updateOne({
-    _id: 1
-},{
+db.products.find({ _id: 1 });
+db.products.updateOne(
+  {
+    _id: 1,
+  },
+  {
     $set: {
-        category: "food"
-    }
-});
+      category: 'food',
+    },
+  }
+);
 
 // update products set category = "food" where _id = 2
-db.products.updateOne({
-    _id: 2
-},{
+db.products.updateOne(
+  {
+    _id: 2,
+  },
+  {
     $set: {
-        category: "food"
-    }
-});
+      category: 'food',
+    },
+  }
+);
 
 // update products set tags = ["food"] where category = "food" and tags is null
-db.products.updateMany({
+db.products.updateMany(
+  {
     $and: [
-        {
-            category :{
-                $eq: "food"
-            }
+      {
+        category: {
+          $eq: 'food',
         },
-        {
-            tags: {
-                $exists: false
-            }
-        }
-    ]
-}, {
+      },
+      {
+        tags: {
+          $exists: false,
+        },
+      },
+    ],
+  },
+  {
     $set: {
-        tags: ["food"]
-    }
-})
+      tags: ['food'],
+    },
+  }
+);
 
 // update products set wrong = "wrong"
 db.products.updateMany({}, [
-    {
-        $set: {
-            wrong: "wrong"
-        }
-    }
+  {
+    $set: {
+      wrong: 'wrong',
+    },
+  },
 ]);
 
 // update products set wrong = null
 db.products.updateMany({}, [
-    {
-        $set: {
-            wrong: null
-        }
-    }
+  {
+    $set: {
+      wrong: null,
+    },
+  },
 ]);
 db.products.updateMany({}, [
-    {
-        $unset: [ "wrong" ] 
-    }
+  {
+    $unset: ['wrong'],
+  },
 ]);
 
 // insert wrong document
 db.products.insertMany([
-    {
-        _id: 9,
-        name: "Ups Salah",
-        wrong: "Salah Lagi"
-    }
+  {
+    _id: 9,
+    name: 'Ups Salah',
+    wrong: 'Salah Lagi',
+  },
 ]);
 
 // replace document with id 9
-db.products.replaceOne({
-    _id: 9
-}, {
-    name: "Adidas Pulseboost HD Running Shoes Sepatu lari Pria",
+db.products.replaceOne(
+  {
+    _id: 9,
+  },
+  {
+    name: 'Adidas Pulseboost HD Running Shoes Sepatu lari Pria',
     price: new NumberLong(1100000),
-    category: "shoes",
-    tags: [
-        "adidas", "shoes", "running"
-    ]
-});
+    category: 'shoes',
+    tags: ['adidas', 'shoes', 'running'],
+  }
+);
